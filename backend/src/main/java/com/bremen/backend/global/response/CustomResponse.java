89a5e03e4319831efe5577ad3da443c8c0323589
On Yoo <dhsdb02@gmail.com>
2024-04-24 15:21:29 +0900

    chore: #16 예외처리용 custom 클래스 추가

---
+++ backend/src/main/java/com/bremen/backend/global/response/CustomResponse.java
@@ -0,0 +1,17 @@
+package com.bremen.backend.global.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class CustomResponse<T> {
+	private int status;
+	private String message;
+	private T data;
+
+}
---
+++ backend/src/main/java/com/bremen/backend/global/response/ErrorCode.java
@@ -0,0 +1,22 @@
+package com.bremen.backend.global.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public enum ErrorCode {
+
+	//400 BAD_REQUEST 잘못된 요청
+	INVALID_PARAMETER(400, "파라미터 값을 확인해주세요."),
+
+	//404 NOT_FOUND 잘못된 리소스 접근
+
+	//409 CONFLICT 중복된 리소스
+
+	//500 INTERNAL SERVER ERROR
+	INTERNAL_SERVER_ERROR(500, "서버 에러입니다. 서버 팀에 연락주세요!");
+
+	private final int status;
+	private final String message;
+}
