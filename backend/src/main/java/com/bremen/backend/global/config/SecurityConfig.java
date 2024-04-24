c7e9a1223eb068bb75c0250eb970588fd8caf191
On Yoo <dhsdb02@gmail.com>
2024-04-24 16:45:46 +0900

    config: #17 시큐리티 관련 config 추가

---
+++ backend/src/main/java/com/bremen/backend/global/config/SecurityConfig.java
@@ -0,0 +1,45 @@
+package com.bremen.backend.global.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+import lombok.RequiredArgsConstructor;
+
+@Configuration
+@EnableWebSecurity(debug = true)
+@RequiredArgsConstructor
+public class SecurityConfig {
+	private final CorsConfig corsConfig;
+	private static final String[] PERMIT_URL_ARRAY = {
+		/* swagger v3 */
+		"/v3/api-docs/**",
+		"/swagger-ui/**"
+	};
+
+	@Bean
+	public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+		httpSecurity
+			.sessionManagement(
+				(session) -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+			)
+			.csrf(AbstractHttpConfigurer::disable)
+			.authorizeHttpRequests(
+				auth -> auth
+					.requestMatchers(PERMIT_URL_ARRAY).permitAll()
+					.requestMatchers(HttpMethod.GET, "/error").permitAll()
+					.requestMatchers(HttpMethod.POST, "/error").permitAll()
+					.anyRequest().authenticated()
+			).cors(
+				cors -> cors.configurationSource(corsConfig.corsConfigurationSource())
+			);
+
+		return httpSecurity.build();
+	}
+
+}
