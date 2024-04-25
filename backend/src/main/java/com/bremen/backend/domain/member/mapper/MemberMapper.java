c78de745d10d829e4a7dbee144af526b9e0754c3
On Yoo <dhsdb02@gmail.com>
2024-04-25 17:01:49 +0900

    feat(member): #21 멤버 request dto mapper 추가

--- backend/src/main/java/com/bremen/backend/domain/member/mapper/MemberMapper.java
+++ backend/src/main/java/com/bremen/backend/domain/member/mapper/MemberMapper.java
@@ -3,6 +3,7 @@ package com.bremen.backend.domain.member.mapper;
 import org.mapstruct.Mapper;
 import org.mapstruct.factory.Mappers;
 
+import com.bremen.backend.domain.member.dto.MemberRequest;
 import com.bremen.backend.domain.member.dto.MemberResponse;
 import com.bremen.backend.domain.member.entity.Member;
 
@@ -11,4 +12,8 @@ public interface MemberMapper {
 	MemberMapper INSTANCE = Mappers.getMapper(MemberMapper.class);
 
 	MemberResponse memberToMemberResponse(Member member);
+
+	Member memberRequestToMember(MemberRequest memberRequest);
+
+	MemberRequest memberToMemberRequest(Member member);
 }
