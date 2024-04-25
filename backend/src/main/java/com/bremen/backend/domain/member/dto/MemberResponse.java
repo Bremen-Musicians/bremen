e2470cd6a849a415a0fc32c3d9b14ef3cc17901f
On Yoo <dhsdb02@gmail.com>
2024-04-25 17:03:06 +0900

    feat(member): #21 data 어노테이션으로 변경

--- backend/src/main/java/com/bremen/backend/domain/member/dto/MemberResponse.java
+++ backend/src/main/java/com/bremen/backend/domain/member/dto/MemberResponse.java
@@ -1,11 +1,12 @@
 package com.bremen.backend.domain.member.dto;
 
-import lombok.Getter;
-import lombok.Setter;
+import lombok.Builder;
+import lombok.Data;
 
-@Getter
-@Setter
+@Data
+@Builder
 public class MemberResponse {
+	private String username;
 	private String nickname;
 	private String introduce;
 	private String profileImage;
