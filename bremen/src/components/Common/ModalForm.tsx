cad444a6b93c5ad58daed755b0efa194edf8a6e5
hyunjeong1 <hjeong.r@gmail.com>
2024-04-25 14:49:30 +0900

    feat: #27 저작권 모달 구현

---
+++ bremen/src/components/Common/ModalForm.tsx
@@ -0,0 +1,30 @@
+import {AiOutlineClose} from 'react-icons/ai';
+import styles from '@/components/Common/ModalForm.module.scss';
+
+const ModalForm = ({
+  children,
+  isOpen,
+  onClose,
+}: {
+  children?: JSX.Element;
+  isOpen: boolean;
+  onClose: () => void;
+}) => {
+  if (!isOpen) return null;
+
+  return (
+    <div>
+      <div className={styles.container}>
+        <div className={styles.Header}>
+          <AiOutlineClose onClick={onClose} />
+        </div>
+        <div className={styles.Body}>
+          <div className={styles.content}>{children}</div>
+        </div>
+      </div>
+      <div className={styles.shadow} onClick={onClose} />
+    </div>
+  );
+};
+
+export default ModalForm;
