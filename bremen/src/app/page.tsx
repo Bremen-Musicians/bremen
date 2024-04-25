619dd80fb008433512cd450da259b79271570717
seonghyeon.moon <seonghyeon.moon@snu.ac.kr>
2024-04-25 10:18:14 +0900

    feat: #22 메인페이지 ui 구현

--- bremen/src/app/page.tsx
+++ bremen/src/app/page.tsx
@@ -1,5 +1,23 @@
 import '@/styles/reset.css';
+import styles from '@/app/page.module.scss';
+import Video from '@/components/Common/Video';
 
 export default function Home() {
-  return <div>나는 홈</div>;
+  return (
+    <>
+      <div className={styles.challenge}>챌린지광고</div>
+      <div className={styles.shortstitle}>쇼츠</div>
+      <div className={styles.videolist}>
+        <Video />
+        <Video />
+        <Video />
+        <Video />
+        <Video />
+        <Video />
+        <Video />
+        <Video />
+        <Video />
+      </div>
+    </>
+  );
 }
