decb3bbaece2cf545a1ec415a12f37a37d4e1db4
On Yoo <dhsdb02@gmail.com>
2024-04-26 10:25:12 +0900

    feat(member): #21 멤버 수정 api 추가

--- backend/src/main/java/com/bremen/backend/domain/member/controller/MemberController.java
+++ backend/src/main/java/com/bremen/backend/domain/member/controller/MemberController.java
@@ -3,6 +3,7 @@ package com.bremen.backend.domain.member.controller;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.bremen.backend.domain.member.dto.MemberRequest;
 import com.bremen.backend.domain.member.dto.MemberResponse;
+import com.bremen.backend.domain.member.dto.MemberUpdateRequest;
 import com.bremen.backend.domain.member.service.MemberService;
 import com.bremen.backend.global.response.CustomResponse;
 
@@ -27,7 +29,7 @@ public class MemberController {
 
 	@GetMapping("/{id}")
 	ResponseEntity<CustomResponse<MemberResponse>> memberDetails(@PathVariable("id") Long id) {
-		MemberResponse memberResponse = memberService.findMember(id);
+		MemberResponse memberResponse = memberService.findMemberById(id);
 		return ResponseEntity.ok(new CustomResponse<>(HttpStatus.OK.value(), "조회 성공", memberResponse));
 	}
 
@@ -35,6 +37,12 @@ public class MemberController {
 	ResponseEntity<CustomResponse<MemberResponse>> memberAdd(@RequestBody MemberRequest memberRequest) {
 		MemberResponse memberResponse = memberService.addMember(memberRequest);
 		return ResponseEntity.ok(new CustomResponse<>(HttpStatus.OK.value(), "유저가 성공적으로 추가되었습니다", memberResponse));
+	}
+
+	@PatchMapping()
+	ResponseEntity<CustomResponse<MemberResponse>> memberModify(@RequestBody MemberUpdateRequest memberUpdateRequest) {
+		MemberResponse memberResponse = memberService.modifyMember(memberUpdateRequest);
+		return ResponseEntity.ok(new CustomResponse<>(HttpStatus.OK.value(), "유저가 성공적으로 수정되었습니다", memberResponse));
 	}
 
 }
