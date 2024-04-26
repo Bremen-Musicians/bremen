5dffc561ca930ed7dc58e633a7d590bfb046e431
On Yoo <dhsdb02@gmail.com>
2024-04-26 16:36:09 +0900

    fix(member): #21 쿼리문 수정

--- backend/src/main/java/com/bremen/backend/domain/member/repository/MemberRepository.java
+++ backend/src/main/java/com/bremen/backend/domain/member/repository/MemberRepository.java
@@ -4,6 +4,7 @@ import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.bremen.backend.domain.member.entity.Member;
@@ -11,6 +12,6 @@ import com.bremen.backend.domain.member.entity.Member;
 @Repository
 public interface MemberRepository extends JpaRepository<Member, Long> {
 
-	@Query("SELECT m FROM Member m WHERE m.id = ?1 AND m.isDeleted = false")
-	Optional<Member> findById(Long id);
+	@Query("SELECT m FROM Member m WHERE m.id = :id AND m.isDeleted = false")
+	Optional<Member> findById(@Param("id") Long id);
 }
