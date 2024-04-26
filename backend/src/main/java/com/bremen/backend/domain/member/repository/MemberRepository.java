790b4a45418a822b3dfcd63c20595eff2739b2f2
On Yoo <dhsdb02@gmail.com>
2024-04-26 16:22:31 +0900

    feat(member): #21 유저 조회 리포지토리 변경

--- backend/src/main/java/com/bremen/backend/domain/member/repository/MemberRepository.java
+++ backend/src/main/java/com/bremen/backend/domain/member/repository/MemberRepository.java
@@ -1,10 +1,16 @@
 package com.bremen.backend.domain.member.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.bremen.backend.domain.member.entity.Member;
 
 @Repository
 public interface MemberRepository extends JpaRepository<Member, Long> {
+
+	@Query("SELECT m FROM Member m WHERE m.id = ?1 AND m.isDeleted = false")
+	Optional<Member> findById(Long id);
 }
