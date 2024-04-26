7d88c79bc9a66e601e4aaf58b52d24db9db21d7c
On Yoo <dhsdb02@gmail.com>
2024-04-26 10:22:55 +0900

    feat(member): #21 멤버 별도 수정 메서드 추가

--- backend/src/main/java/com/bremen/backend/domain/member/entity/Member.java
+++ backend/src/main/java/com/bremen/backend/domain/member/entity/Member.java
@@ -6,6 +6,8 @@ import org.hibernate.annotations.ColumnDefault;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
+import com.bremen.backend.domain.member.dto.MemberUpdateRequest;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -13,10 +15,12 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotNull;
+import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Table(name = "member")
 @Entity
@@ -29,16 +33,25 @@ public class Member {
 	@Column(name = "id")
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+
 	@NotNull
 	@Column(unique = true)
 	private String username;
 
+	@Setter(AccessLevel.PROTECTED)
 	private String password;
 
+	@Setter(AccessLevel.PROTECTED)
 	private String introduce;
+
 	@NotNull
+	@Column(unique = true)
+	@Setter(AccessLevel.PROTECTED)
 	private String nickname;
+
+	@Setter(AccessLevel.PROTECTED)
 	private String profileImage;
+
 	@ColumnDefault("0")
 	private int followerCnt;
 
@@ -47,7 +60,7 @@ public class Member {
 
 	@ColumnDefault("false")
 	@NotNull
-	@Column(name = "deleted", columnDefinition = "TINYINT(1)")
+	@Column(name = "is_deleted", columnDefinition = "TINYINT(1)")
 	private boolean isDeleted;
 
 	@CreationTimestamp
@@ -56,4 +69,14 @@ public class Member {
 	private LocalDateTime deleteTime;
 	@UpdateTimestamp
 	private LocalDateTime updateTime;
+
+	public void modifyPassword(String password) {
+		this.password = password;
+	}
+
+	public void modifyUserInformation(MemberUpdateRequest memberUpdateRequest) {
+		this.profileImage = memberUpdateRequest.getProfileImage();
+		this.introduce = memberUpdateRequest.getIntroduce();
+		this.nickname = memberUpdateRequest.getNickname();
+	}
 }
