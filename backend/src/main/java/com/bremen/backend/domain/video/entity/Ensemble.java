e8a5cc6fc5effa59c484833ccd99d46f2a8cea68
전경향 <wjsrudgid@naver.com>
2024-04-29 14:40:54 +0900

    fix: #40 합주 칼럼 변경

--- backend/src/main/java/com/bremen/backend/domain/video/entity/Ensemble.java
+++ backend/src/main/java/com/bremen/backend/domain/video/entity/Ensemble.java
@@ -2,9 +2,12 @@ package com.bremen.backend.domain.video.entity;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -23,9 +26,11 @@ public class Ensemble {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 
-	// @JoinColumn(name = "owner_id");
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "owner_id")
 	private Video owner;
 
-	// @JoinColumn(name = "participant_id")
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "participant_id")
 	private Video participant;
 }
