aa82275155673ced4b68a2ddd4c7cd604b216032
On Yoo <dhsdb02@gmail.com>
2024-04-29 13:20:02 +0900

    feat(member): #38 회원가입 api 추가
    - 회원가입 api
    - 유저 프로필 api

--- backend/src/main/java/com/bremen/backend/domain/user/controller/UserController.java
+++ backend/src/main/java/com/bremen/backend/domain/user/controller/UserController.java
@@ -11,9 +11,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.bremen.backend.domain.user.dto.UserRequest;
+import com.bremen.backend.domain.user.dto.UserProfileRequest;
+import com.bremen.backend.domain.user.dto.UserRegistrationRequest;
 import com.bremen.backend.domain.user.dto.UserResponse;
 import com.bremen.backend.domain.user.dto.UserUpdateRequest;
+import com.bremen.backend.domain.user.service.ProfileService;
+import com.bremen.backend.domain.user.service.RegistrationService;
 import com.bremen.backend.domain.user.service.UserService;
 import com.bremen.backend.global.response.CustomResponse;
 
@@ -27,6 +30,8 @@ import lombok.extern.slf4j.Slf4j;
 public class UserController {
 
 	private final UserService userService;
+	private final RegistrationService registrationService;
+	private final ProfileService profileService;
 
 	@GetMapping()
 	ResponseEntity<CustomResponse<UserResponse>> userDetails(@RequestParam("id") Long id) {
@@ -35,9 +40,16 @@ public class UserController {
 	}
 
 	@PostMapping()
-	ResponseEntity<CustomResponse<UserResponse>> userAdd(@RequestBody UserRequest userRequest) {
-		UserResponse userResponse = userService.addUser(userRequest);
+	ResponseEntity<CustomResponse<UserResponse>> userAdd(@RequestBody UserRegistrationRequest userRegistrationRequest) {
+		UserResponse userResponse = registrationService.register(userRegistrationRequest);
 		return ResponseEntity.ok(new CustomResponse<>(HttpStatus.OK.value(), "유저가 성공적으로 추가되었습니다", userResponse));
+	}
+
+	@PostMapping("/profile")
+	ResponseEntity<CustomResponse<Void>> userProfileAdd(@RequestBody UserProfileRequest userProfileRequest) {
+		profileService.modifyUserProfile(userProfileRequest);
+		CustomResponse<Void> response = new CustomResponse<>(HttpStatus.OK.value(), "회원가입이 성공적으로 완료되었습니다.", null);
+		return new ResponseEntity<>(response, HttpStatus.OK);
 	}
 
 	@PatchMapping()
