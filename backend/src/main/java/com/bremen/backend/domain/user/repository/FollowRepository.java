1c77e9a65f2df6a5237cbf5a762b1e5d908cddda
On Yoo <dhsdb02@gmail.com>
2024-05-03 09:19:16 +0900

    feat(member): #54 팔로우 여부 확인 메서드 추가

--- backend/src/main/java/com/bremen/backend/domain/user/repository/FollowRepository.java
+++ backend/src/main/java/com/bremen/backend/domain/user/repository/FollowRepository.java
@@ -1,7 +1,5 @@
 package com.bremen.backend.domain.user.repository;
 
-import java.util.Optional;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -9,6 +7,9 @@ import com.bremen.backend.domain.user.entity.Follow;
 import com.bremen.backend.domain.user.entity.User;
 
 @Repository
-public interface FollowRepository extends JpaRepository<Follow,Long> {
-	Optional<Follow> deleteFollowByFollowAndFollower(User Follow,User Follower);
+public interface FollowRepository extends JpaRepository<Follow, Long> {
+
+	void deleteFollowByFollowAndFollower(User Follow, User Follower);
+
+	boolean existsFollowByFollowAndFollower(User Follow, User Follower);
 }
