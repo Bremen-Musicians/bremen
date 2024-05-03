1803525eec87c4447293b12fafc35c13573421c0
onyoo <dhsdb02@gmail.com>
2024-05-03 20:56:43 +0900

    config(kafka): #73 참조하는 라이브러리 변경

--- backend/src/main/java/com/bremen/backend/global/config/KafkaProducerConfig.java
+++ backend/src/main/java/com/bremen/backend/global/config/KafkaProducerConfig.java
@@ -4,6 +4,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
@@ -11,10 +12,9 @@ import org.springframework.kafka.annotation.EnableKafka;
 import org.springframework.kafka.core.DefaultKafkaProducerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.core.ProducerFactory;
+import org.springframework.kafka.support.serializer.JsonSerializer;
 
 import com.bremen.backend.domain.notification.message.NotificationMessage;
-import com.fasterxml.jackson.databind.JsonSerializer;
-import com.fasterxml.jackson.databind.ser.std.StringSerializer;
 
 import lombok.RequiredArgsConstructor;
 
@@ -34,7 +34,7 @@ public class KafkaProducerConfig {
 	}
 
 	@Bean
-	public KafkaTemplate<String, NotificationMessage> kafkaTemplate() {
+	public KafkaTemplate<String, NotificationMessage> kafkaProducer() {
 		return new KafkaTemplate<>(producerFactory());
 	}
 }
