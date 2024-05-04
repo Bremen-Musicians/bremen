72ca6070c5da279e80de346cc8f68875b7b6c82f
전경향 <wjsrudgid@naver.com>
2024-05-04 16:06:24 +0900

    feat(article): #76 heart entity 추가

---
+++ backend/src/main/java/com/bremen/backend/domain/article/entity/Heart.java
@@ -0,0 +1,38 @@
+package com.bremen.backend.domain.article.entity;
+
+import com.bremen.backend.domain.user.entity.User;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "heart")
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Heart {
+	@Id
+	@Column(name = "id")
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "article_id")
+	private Article article;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "member_id")
+	private User user;
+}
