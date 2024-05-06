e327447191659660519e223d90ac423969273fef
전경향 <wjsrudgid@naver.com>
2024-05-06 15:59:46 +0900

    feat(article): #79 댓글 entity

---
+++ backend/src/main/java/com/bremen/backend/domain/article/entity/Comment.java
@@ -0,0 +1,73 @@
+package com.bremen.backend.domain.article.entity;
+
+import java.time.LocalDateTime;
+
+import org.hibernate.annotations.ColumnDefault;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import com.bremen.backend.domain.user.entity.User;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotNull;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "comment")
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Comment {
+	@Id
+	@Column(name = "id")
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@ManyToOne
+	@JoinColumn(name = "group_id")
+	private Comment group;
+
+	@NotNull
+	@Column(name = "content")
+	private String content;
+
+	@NotNull
+	@ColumnDefault("false")
+	@Setter(AccessLevel.PROTECTED)
+	@Column(name = "is_deleted", columnDefinition = "TINYINT(1)")
+	private boolean isDeleted;
+
+	@CreationTimestamp
+	@Column(name = "create_time")
+	private LocalDateTime createTime;
+
+	@Column(name = "delete_time")
+	@Setter(AccessLevel.PROTECTED)
+	private LocalDateTime deleteTime;
+
+	@UpdateTimestamp
+	@Column(name = "update_time")
+	private LocalDateTime updateTime;
+
+	@JoinColumn(name = "article_id")
+	@ManyToOne(fetch = FetchType.LAZY, optional = false)
+	private Article article;
+
+	@JoinColumn(name = "member_id")
+	@ManyToOne(fetch = FetchType.LAZY, optional = false)
+	private User user;
+}
