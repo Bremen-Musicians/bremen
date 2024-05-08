3f063f8328829d1f0c834be17ffaba3caf745ce2
seonghyeon.moon <seonghyeon.moon@snu.ac.kr>
2024-05-08 12:59:39 +0900

    feat: #26 프로필이미지 컴포넌트화

---
+++ bremen/src/components/Common/ProfileImage.tsx
@@ -0,0 +1,37 @@
+import {useRouter} from 'next/navigation';
+import Image from 'next/image';
+import useUserInfoStore from '@/stores/UserInfo';
+// import styles from './ProfileImage.module.scss';
+
+interface IProfileImage {
+  userNickname: string;
+  profileImage: string;
+}
+
+export default function ProfileImage({
+  userNickname,
+  profileImage,
+}: IProfileImage) {
+  const router = useRouter();
+  const {zustandUserNickname} = useUserInfoStore();
+
+  const moveToProfile = () => {
+    // 전역으로 관리하는 내 정보랑 비교해 userNickname와
+    // 내 nickname(zustandUserNickname)이 같으면 /mypage로 보내야 한다.
+    if (zustandUserNickname !== '' && userNickname === zustandUserNickname) {
+      router.push('/mypage');
+    } else {
+      router.push(`/user/${userNickname}`);
+    }
+  };
+
+  return (
+    <div>
+      <Image
+        src={profileImage}
+        alt="profileImage"
+        onClick={() => moveToProfile}
+      />
+    </div>
+  );
+}
