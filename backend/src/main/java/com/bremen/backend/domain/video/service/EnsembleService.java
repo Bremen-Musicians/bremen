8e10bc8eb660f29096546f993e18148e59bc54fa
On Yoo <dhsdb02@gmail.com>
2024-05-08 17:37:06 +0900

    feat(video): #91 합주 추가 및 조회 서비스 추가

---
+++ backend/src/main/java/com/bremen/backend/domain/video/service/EnsembleService.java
@@ -0,0 +1,12 @@
+package com.bremen.backend.domain.video.service;
+
+import java.util.List;
+
+import com.bremen.backend.domain.video.entity.Video;
+
+public interface EnsembleService {
+	void addEnsemble(Video owner, Video participant);
+
+	//소유자의 동영상을 참조한 모든 동영상을 가져옵니다
+	List<Video> getRelatedVideoList(Video owner);
+}
