ede2946f3512ed3a804f9675c360f378c70b5592
On Yoo <dhsdb02@gmail.com>
2024-05-08 17:36:47 +0900

    feat(video): #91 합주 리포지토리 추가

---
+++ backend/src/main/java/com/bremen/backend/domain/video/repository/EnsembleRepository.java
@@ -0,0 +1,19 @@
+package com.bremen.backend.domain.video.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import com.bremen.backend.domain.video.entity.Ensemble;
+
+@Repository
+public interface EnsembleRepository extends JpaRepository<Ensemble, Long> {
+
+	@Query("SELECT e from Ensemble e where e.owner = :videoId")
+	List<Ensemble> findByOwner(Long videoId);
+
+	@Query("SELECT e from Ensemble e where e.participant = :videoId")
+	List<Ensemble> findByParticipant(Long videoId);
+}
