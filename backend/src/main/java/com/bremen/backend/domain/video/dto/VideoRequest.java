82de54d35de81ad5f92243a8d3998070873f97b8
On Yoo <dhsdb02@gmail.com>
2024-05-08 17:34:04 +0900

    feat(video): #91 합주 동영상 리스트 추가

--- backend/src/main/java/com/bremen/backend/domain/video/dto/VideoRequest.java
+++ backend/src/main/java/com/bremen/backend/domain/video/dto/VideoRequest.java
@@ -1,5 +1,7 @@
 package com.bremen.backend.domain.video.dto;
 
+import java.util.List;
+
 import jakarta.validation.constraints.NotNull;
 import lombok.Builder;
 import lombok.Data;
@@ -9,6 +11,8 @@ import lombok.Data;
 public class VideoRequest {
 	@NotNull(message = "합주 여부는 선택해주셔야 합니다.")
 	private boolean isEnsemble;
+
+	private List<Long> ensembleList;
 
 	private Long musicId;
 
