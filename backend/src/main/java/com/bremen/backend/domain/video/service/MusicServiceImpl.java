7bee4589bdacd524ba9c1e61d8e6dc0443f640cf
전경향 <wjsrudgid@naver.com>
2024-05-09 09:29:57 +0900

    feat: #94 음악 제목 검색

--- backend/src/main/java/com/bremen/backend/domain/video/service/MusicServiceImpl.java
+++ backend/src/main/java/com/bremen/backend/domain/video/service/MusicServiceImpl.java
@@ -1,8 +1,12 @@
 package com.bremen.backend.domain.video.service;
 
+import java.util.List;
+
 import org.springframework.stereotype.Service;
 
+import com.bremen.backend.domain.video.dto.MusicResponse;
 import com.bremen.backend.domain.video.entity.Music;
+import com.bremen.backend.domain.video.mapper.MusicMapper;
 import com.bremen.backend.domain.video.repository.MusicRepository;
 import com.bremen.backend.global.CustomException;
 import com.bremen.backend.global.response.ErrorCode;
@@ -17,5 +21,11 @@ public class MusicServiceImpl implements MusicService {
 	@Override
 	public Music getMusicById(Long musicId) {
 		return musicRepository.findById(musicId).orElseThrow(() -> new CustomException(ErrorCode.NOT_FOUND_MUSIC));
+	}
+
+	@Override
+	public List<MusicResponse> searchMusicsByTitle(String keyword) {
+		List<Music> musics = musicRepository.findByTitleContaining(keyword);
+		return musics.stream().map(MusicMapper.INSTANCE::musicToMusicResponse).toList();
 	}
 }
