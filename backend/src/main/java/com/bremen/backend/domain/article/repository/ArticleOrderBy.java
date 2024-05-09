fc548eb6314b077abcc2ad11fbb8860f75b08c6c
onyoo <dhsdb02@gmail.com>
2024-05-09 22:05:32 +0900

    feat(article): #93 정렬을 위한 enum 클래스

---
+++ backend/src/main/java/com/bremen/backend/domain/article/repository/ArticleOrderBy.java
@@ -0,0 +1,32 @@
+package com.bremen.backend.domain.article.repository;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.hibernate.query.Order;
+
+import com.bremen.backend.domain.article.entity.QArticle;
+import com.querydsl.core.types.OrderSpecifier;
+
+public enum ArticleOrderBy {
+	LATEST {
+		@Override
+		public List<OrderSpecifier> getOrderSpecifier(QArticle qArticle) {
+			List<OrderSpecifier> list = new ArrayList<>();
+			list.add(qArticle.createTime.desc());
+			return list;
+		}
+	},
+	POPULAR {
+		@Override
+		public List<OrderSpecifier> getOrderSpecifier(QArticle qArticle) {
+			List<OrderSpecifier> list = new ArrayList<>();
+			list.add(qArticle.likeCnt.desc());
+			list.add(qArticle.hitCnt.desc());
+			list.add(qArticle.createTime.desc());
+			return list;
+		}
+	};
+
+	public abstract List<OrderSpecifier> getOrderSpecifier(QArticle qArticle);
+}
