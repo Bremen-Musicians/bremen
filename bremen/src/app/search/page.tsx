776d07e2f1c61d888ffd5bdc26f34063c05c475e
unknown <qorwlsrb01@gmail.com>
2024-05-10 17:19:48 +0900

    feat: #118 검색 메인 페이지 제작

--- bremen/src/app/search/page.tsx
+++ bremen/src/app/search/page.tsx
@@ -1,9 +1,26 @@
+import Image from 'next/image';
+import styles from '@/app/search/page.module.scss';
 import SearchBar from '@/components/Search/SearchBar';
+import bremenStand2 from '../../../public/bremenstand2.png';
 
 export default function Page() {
   return (
-    <div>
-      <SearchBar />
-    </div>
+    <>
+      <div className={styles.topImageContainer}>
+        <Image
+          className={styles.topImage}
+          src={bremenStand2}
+          alt="bremen"
+          width={150}
+          height={150}
+        />
+      </div>
+      <div>
+        <SearchBar initialValue="" />
+      </div>
+      <div className={styles.infoContainer}>
+        <div className={styles.info}>노래 제목, 아티스트 명으로 검색하세요</div>
+      </div>
+    </>
   );
 }
--- bremen/src/components/Search/SearchBar.tsx
+++ bremen/src/components/Search/SearchBar.tsx
@@ -1,16 +1,47 @@
-import styles from '@/components/Search/SearchBar.module.scss';
-import {FaArrowLeft} from 'react-icons/fa6';
+'use client';
+
+import {useState} from 'react';
+import {useRouter} from 'next/navigation';
 import {HiOutlineSearch} from 'react-icons/hi';
 import {CgClose} from 'react-icons/cg';
+import styles from '@/components/Search/SearchBar.module.scss';
 
-export default function SearchBar() {
+interface SearchBarProps {
+  initialValue: string;
+}
+
+export default function SearchBar({initialValue}: SearchBarProps) {
+  const [inputValue, setInputValue] = useState(initialValue);
+  const router = useRouter(); // useRouter 훅 추가
+
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setInputValue(event.target.value);
+  };
+
+  const handleKeyPress = (event: React.KeyboardEvent<HTMLInputElement>) => {
+    if (event.key === 'Enter' && inputValue.trim()) {
+      // router.push로 라우팅 수행
+      router.push(`/search/${inputValue.trim()}`);
+    }
+  };
+
+  const clearInput = () => {
+    setInputValue('');
+  };
+
   return (
     <div className={styles.searchbar}>
-      <FaArrowLeft className={styles.icons} />
       <div className={styles.searchbox}>
         <HiOutlineSearch className={styles.icons} />
-        <input className={styles.inputbox} type="text"></input>
-        <CgClose className={styles.icons} />
+        <input
+          className={styles.inputbox}
+          type="text"
+          value={inputValue}
+          onChange={handleInputChange}
+          onKeyPress={handleKeyPress}
+          placeholder="Search..."
+        />
+        <CgClose className={styles.icons} onClick={clearInput} />
       </div>
     </div>
   );
