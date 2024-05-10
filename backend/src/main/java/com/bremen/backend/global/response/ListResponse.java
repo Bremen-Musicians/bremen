758563012f188b3525f204ba45c1cde4126516d7
On Yoo <dhsdb02@gmail.com>
2024-05-10 10:28:57 +0900

    feat(article): #103 응답객체 수정

--- backend/src/main/java/com/bremen/backend/global/response/ListResponse.java
+++ backend/src/main/java/com/bremen/backend/global/response/ListResponse.java
@@ -2,6 +2,8 @@ package com.bremen.backend.global.response;
 
 import java.util.List;
 
+import org.springframework.data.domain.Pageable;
+
 import lombok.Getter;
 import lombok.Setter;
 
@@ -9,16 +11,20 @@ import lombok.Setter;
 @Setter
 public class ListResponse<T> extends CustomResponse {
 	List<T> items;
-	int size;
+	Pageable pageable;
+	Long size;
 
-	public ListResponse(int status, String message, List<T> items, int size) {
+	public ListResponse(int status, String message, List<T> items, Long size, Pageable pageable) {
 		super(status, message);
+		this.pageable = pageable;
 		this.items = items;
 		this.size = size;
 	}
 
-	public ListResponse(List<T> items, int size) {
+	public ListResponse(List<T> items, Long size, Pageable pageable) {
 		this.items = items;
 		this.size = size;
+		this.pageable = pageable;
 	}
+
 }
