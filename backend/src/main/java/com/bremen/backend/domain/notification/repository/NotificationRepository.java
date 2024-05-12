9b56ecffc39a43a65fb8e5f49f4bddf10aab13e2
onyoo <dhsdb02@gmail.com>
2024-05-12 19:08:13 +0900

    feat(alarm): #73 알림 리포지토리 추가

---
+++ backend/src/main/java/com/bremen/backend/domain/notification/repository/NotificationRepository.java
@@ -0,0 +1,16 @@
+package com.bremen.backend.domain.notification.repository;
+
+import java.util.List;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import com.bremen.backend.domain.notification.entity.Notification;
+
+public interface NotificationRepository extends JpaRepository<Notification,Long> {
+	@Query("SELECT n FROM Notification n where n.user.username =:username and n.isDeleted = false")
+	Page<Notification> findByUser(@Param("username") String username, Pageable pageable);
+}
