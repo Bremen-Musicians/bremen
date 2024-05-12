ceb33d177a86a4cc68c56f44fa8ddf25c9da3a38
onyoo <dhsdb02@gmail.com>
2024-05-12 19:10:07 +0900

    feat(alarm): #73 알림 api 추가

--- backend/src/main/java/com/bremen/backend/domain/notification/controller/NotificationController.java
+++ backend/src/main/java/com/bremen/backend/domain/notification/controller/NotificationController.java
@@ -1,12 +1,19 @@
 package com.bremen.backend.domain.notification.controller;
 
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
+import com.bremen.backend.domain.article.repository.ArticleOrderBy;
+import com.bremen.backend.domain.notification.service.EmitterService;
 import com.bremen.backend.domain.notification.service.NotificationService;
+import com.bremen.backend.global.response.ListResponse;
 
 import lombok.RequiredArgsConstructor;
 
@@ -14,11 +21,20 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/notification")
 public class NotificationController {
-	private final NotificationService emitterService;
+	private final EmitterService emitterService;
+	private final NotificationService notificationService;
 
 	@GetMapping("/subscribe")
-	public SseEmitter subscribe(Authentication authentication) {
+	SseEmitter subscribe(Authentication authentication) {
 		return emitterService.connectAlarm(authentication.getPrincipal().toString());
+	}
+
+	@GetMapping()
+	ResponseEntity<ListResponse> getNotification(Pageable pageable){
+		ListResponse listResponse = notificationService.getNotification(pageable);
+		listResponse.setStatus(HttpStatus.OK.value());
+		listResponse.setMessage("알림 조회 성공");
+		return ResponseEntity.ok(listResponse);
 	}
 
 }
