a80a3bb71caa3bdd7645f916590d6e2c2c4992bd
On Yoo <dhsdb02@gmail.com>
2024-05-13 10:03:55 +0900

    feat(profile): #124 유저 프로필 dto에 멀티파트 컬럼 추가

--- backend/src/main/java/com/bremen/backend/domain/user/dto/UserProfileRequest.java
+++ backend/src/main/java/com/bremen/backend/domain/user/dto/UserProfileRequest.java
@@ -1,5 +1,7 @@
 package com.bremen.backend.domain.user.dto;
 
+import org.springframework.web.multipart.MultipartFile;
+
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
@@ -9,6 +11,8 @@ public class UserProfileRequest {
 	@NotBlank(message = "아이디는 비어있을 수 없습니다.")
 	@Email
 	private String username;
-	private String profileImage;
+	private String profileUrl;
+	private MultipartFile profileImage;
 	private String introduce;
+
 }
