f4fabb6d6865a2141500fc49bd0a272b90b8a5a3
전경향 <wjsrudgid@naver.com>
2024-05-15 16:31:36 +0900

    fix(challenge): #129 챌린지 게시글 조회 query 오사용된 entity명 수정

--- backend/src/main/java/com/bremen/backend/domain/article/repository/ArticleChallengeQueryDslRepository.java
+++ backend/src/main/java/com/bremen/backend/domain/article/repository/ArticleChallengeQueryDslRepository.java
@@ -1,2 +1,44 @@
-package com.bremen.backend.domain.article.repository;public class ArticleChallengeQueryDslRepository {
+package com.bremen.backend.domain.article.repository;
+
+import static com.bremen.backend.domain.article.entity.QArticle.*;
+import static com.bremen.backend.domain.article.repository.ArticleOrderBy.*;
+import static com.bremen.backend.domain.challenge.entity.QChallengeArticle.*;
+import static com.bremen.backend.domain.user.entity.QUser.*;
+import static com.bremen.backend.domain.video.entity.QVideo.*;
+
+import java.util.List;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.support.PageableExecutionUtils;
+import org.springframework.stereotype.Repository;
+
+import com.bremen.backend.domain.article.entity.Article;
+import com.querydsl.core.types.OrderSpecifier;
+import com.querydsl.jpa.JPQLQuery;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+
+import lombok.RequiredArgsConstructor;
+
+@Repository
+@RequiredArgsConstructor
+public class ArticleChallengeQueryDslRepository {
+	private final JPAQueryFactory queryFactory;
+
+	public Page<Article> findChallengeArticle(Long challengeId, Long instrumentId, Pageable pageable) {
+		JPQLQuery<Article> query = queryFactory
+			.selectFrom(article)
+			.join(article.video, video).fetchJoin()
+			.join(article.user, user).fetchJoin()
+			.join(challengeArticle).on(article.id.eq(challengeArticle.article.id))
+			.where(
+				challengeArticle.challenge.id.eq(challengeId),
+				instrumentId != null ? article.video.instrument.id.eq(instrumentId) : null
+			)
+			.orderBy(POPULAR.getOrderSpecifier(article).toArray(OrderSpecifier[]::new));
+
+		List<Article> articles = query.offset(pageable.getOffset()).limit(pageable.getPageSize()).fetch();
+
+		return PageableExecutionUtils.getPage(articles, pageable, query::fetchCount);
+	}
 }
