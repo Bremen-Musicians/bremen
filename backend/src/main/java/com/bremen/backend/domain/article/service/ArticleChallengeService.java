cfc9643b91a7c18c3744fddf3bd4d05cbdedbec2
전경향 <wjsrudgid@naver.com>
2024-05-15 16:33:05 +0900

    feat: #129 특정 챌린지 참여 게시글 조회 서비스

--- backend/src/main/java/com/bremen/backend/domain/article/service/ArticleChallengeService.java
+++ backend/src/main/java/com/bremen/backend/domain/article/service/ArticleChallengeService.java
@@ -1,2 +1,10 @@
-package com.bremen.backend.domain.article.service;public interface ArticleChallengeService {
+package com.bremen.backend.domain.article.service;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import com.bremen.backend.domain.article.dto.ArticleListResponse;
+
+public interface ArticleChallengeService {
+	Page<ArticleListResponse> findArticlesByChallengeId(Long challengeId, Long InstrumentId, Pageable pageable);
 }
