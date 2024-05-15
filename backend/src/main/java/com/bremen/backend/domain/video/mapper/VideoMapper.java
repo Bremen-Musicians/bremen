4b6851dd04c7e834fa5181a18a84d6b5bb17e3c9
전경향 <wjsrudgid@naver.com>
2024-05-15 16:16:27 +0900

    feat: 게시글 리스트 response를 위한 mapper 생성

--- backend/src/main/java/com/bremen/backend/domain/video/mapper/VideoMapper.java
+++ backend/src/main/java/com/bremen/backend/domain/video/mapper/VideoMapper.java
@@ -6,6 +6,7 @@ import org.mapstruct.factory.Mappers;
 
 import com.bremen.backend.domain.video.dto.VideoRequest;
 import com.bremen.backend.domain.video.dto.VideoResponse;
+import com.bremen.backend.domain.video.dto.VideoUrlResponse;
 import com.bremen.backend.domain.video.entity.Video;
 
 @Mapper
@@ -21,4 +22,6 @@ public interface VideoMapper {
 	@Mapping(target = "musicId", source = "video.music.id")
 	@Mapping(target = "instrumentId", source = "video.instrument.id")
 	VideoRequest videoToVideoRequest(Video video);
+
+	VideoUrlResponse videoToVideoUrlResponse(Video video);
 }
