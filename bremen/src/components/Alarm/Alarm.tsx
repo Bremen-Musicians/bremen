5f4ed20b9c92204762bc086a5719da563ce07cd4
jangtaesoo <use02042@naver.com>
2024-05-16 16:58:48 +0900

    feat: #100 build 오류로 수정

--- bremen/src/components/Alarm/Alarm.tsx
+++ bremen/src/components/Alarm/Alarm.tsx
@@ -1,15 +1,25 @@
-import React, { useState, useEffect } from 'react';
+/* eslint-disable no-console */
+/* eslint-disable @typescript-eslint/no-floating-promises */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+import React, {useState, useEffect} from 'react';
 import Link from 'next/link';
 import Image from 'next/image';
 import styles from '@/components/Alarm/Alarm.module.scss';
 import Header from '@/components/Common/Header';
-import Image from 'next/image';
 import api from '@/api/api';
 import 'moment/locale/ko';
 
 interface Alarm {
   id: number;
-  page: number;
+  content: string;
+  type: string;
+  createTime: string;
+}
+
+interface NotificationItem {
+  id: number;
   content: string;
   type: string;
   createTime: string;
@@ -19,20 +29,20 @@ const Page = () => {
   const [alarmList, setAlarmList] = useState<Alarm[]>([]);
   const [showHeader, setShowHeader] = useState(false);
 
-  const handleDeleteAlarm = async (id: number) => {
+  const handleDeleteAlarm = (id: number) => {
     try {
       // 클라이언트에서 삭제
       const newAlarmList = alarmList.filter(alarm => alarm.id !== id);
       setAlarmList(newAlarmList);
-  
       // 서버에 삭제 요청
-      await api.delete(`/notification?id=${id}`);
-      console.log('알림 삭제 성공');
+      api.delete(`/notification?id=${id}`).catch(error => {
+        console.error('알림 삭제 중 에러 발생:', error);
+      });
+      // console.log('알림 삭제 성공');
     } catch (error) {
       console.error('알림 삭제 중 에러 발생:', error);
     }
   };
-  
 
   const handleDeleteAll = () => {
     setAlarmList([]);
@@ -40,7 +50,9 @@ const Page = () => {
 
   const calculateElapsedTime = (date: string) => {
     const now = new Date();
-    const diff = Math.round((now.getTime() - new Date(date).getTime()) / (1000 * 60)); // 경과 시간을 분 단위로 계산
+    const diff = Math.round(
+      (now.getTime() - new Date(date).getTime()) / (1000 * 60),
+    ); // 경과 시간을 분 단위로 계산
 
     if (diff < 60) {
       return `${diff}분 전`;
@@ -73,15 +85,17 @@ const Page = () => {
     const fetchNotifications = async () => {
       try {
         const response = await api.get('/notification');
-        console.log('알림 데이터:', response.data);
+        // console.log('알림 데이터:', response.data);
         // Alarm 인터페이스에 맞게 데이터 매핑
-        const mappedAlarms = response.data.items.map((item: any) => ({
-          id: item.id, // 인덱스를 사용하여 유일한 id 생성
-          page: response.data.pageable.pageNumber,
-          content: item.content,
-          type: item.type,
-          createTime: item.createTime,
-        }));
+        const mappedAlarms = response.data.items.map(
+          (item: NotificationItem) => ({
+            id: item.id,
+            content: item.content,
+            type: item.type,
+            createTime: item.createTime,
+          }),
+        );
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
         setAlarmList(mappedAlarms); // 알림 데이터를 상태로 설정
       } catch (error) {
         console.error('알림 데이터를 가져오는 중 에러 발생:', error);
@@ -100,7 +114,7 @@ const Page = () => {
         <div className={styles.alarmtitle}>
           <Link href="/alarm" className={styles.alarmtext}>
             알림
-          </Link> 
+          </Link>
         </div>
         <div className={styles.deleteAllButtonBlock}>
           <button className={styles.deleteAllButton} onClick={handleDeleteAll}>
@@ -109,7 +123,7 @@ const Page = () => {
         </div>
         <div
           className={styles.alarms}
-          style={{ maxHeight: '300px', overflowY: 'auto' }}
+          style={{maxHeight: '300px', overflowY: 'auto'}}
         >
           {alarmList.length === 0 ? (
             <div className={styles.noAlarms}>현재 받은 알림이 없습니다.</div>
@@ -124,7 +138,12 @@ const Page = () => {
                   className={styles.deleteButton}
                   onClick={() => handleDeleteAlarm(alarm.id)}
                 >
-                  <Image src={'/Icon/minus.png'} alt="삭제" width={20} height={20} />
+                  <Image
+                    src={'/Icon/minus.png'}
+                    alt="삭제"
+                    width={20}
+                    height={20}
+                  />
                 </button>
               </div>
             ))
