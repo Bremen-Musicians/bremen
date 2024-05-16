21e43a233762b118bdfeb4b68ecd48523d37e3f5
On Yoo <dhsdb02@gmail.com>
2024-05-16 09:50:37 +0900

    feat(article): #142 챌린지 게시글 서비스 추가

---
+++ backend/src/main/java/com/bremen/backend/domain/challenge/service/ChallengeArticleService.java
@@ -0,0 +1,7 @@
+package com.bremen.backend.domain.challenge.service;
+
+import com.bremen.backend.domain.article.entity.Article;
+
+public interface ChallengeArticleService {
+	void addChallengeArticle(Article article);
+}
