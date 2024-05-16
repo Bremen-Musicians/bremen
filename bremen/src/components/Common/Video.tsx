35f1b3a8aad7d9caa30b1154ee1853ad6c436894
seonghyeon.moon <seonghyeon.moon@snu.ac.kr>
2024-05-16 09:11:53 +0900

    feat: #136 메인페이지 게시글 목록 api 연결 및 무한스크롤 적용

--- bremen/src/components/Common/Video.tsx
+++ bremen/src/components/Common/Video.tsx
@@ -3,10 +3,10 @@
 import {useRouter} from 'next/navigation';
 import styles from '@/components/Common/Video.module.scss';
 
-export default function Video({id, title}: {id: number, title: string}) {
+export default function Video({id, title, videoUrl}: {id: number, title: string, videoUrl: string}) {
   const router = useRouter();
   const viewVideo = () => {
-    router.push(`/detail/2`);
+    router.push(`/detail/${id}`);
   };
 
   return (
--- bremen/src/components/Main/index.tsx
+++ bremen/src/components/Main/index.tsx
@@ -8,22 +8,31 @@ import Header from '@/components/Common/Header';
 import { useEffect, useState } from 'react';
 import api from '@/api/api';
 import {IMainResponse, IArticleList} from '@/types/ArticleListResponse'
+import { useInView } from 'react-intersection-observer';
 
 export default function Home() {
   const [order, setOrder] = useState<string>('POPULAR');
   const [page, setPage] = useState<number>(0);
-  const [size, setSize] = useState<number>(10);
   const [list, setList] = useState<IArticleList[]>([]);
-  // const [ref, inView] = useInView();
+  const [ref, inView] = useInView();
 
-  useEffect(() => {
-    api.get<IMainResponse>(`/articles/feed?order=${order}&page=${page}&size=${size}&sort=string`).then((response) => {
+  const feedAPI = () => {
+    api.get<IMainResponse>(`/articles/feed?order=${order}&page=${page}&size=12`).then((response) => {
       const listData = response.data.items;
-      setList(listData);
+      setList((prevList) => [...prevList, ...listData]);
     }).catch(error => {
       console.error('에러!', error);
     })
-  }, [])
+  }
+
+  // 페이지 하단 감지
+  useEffect(() => {
+    if (inView) {
+      feedAPI();
+      setPage(page+1);
+    }
+  }, [inView])
+
 
   return (
     <>
@@ -32,9 +41,9 @@ export default function Home() {
         <div className={styles.videocontainer}>
           <div className={styles.shortstitle}>쇼츠</div>
           <div className={styles.videolist}>
-            {list.map((video, key) => <Video id={video.id} title={video.title} />)}
+            {list.map((video, key) => <Video key={key} id={video.id} title={video.title} videoUrl={video.videoUrl} />)}
           </div>
-          <div className={styles.videomargin}></div>
+          <div ref={ref} className={styles.videomargin}></div>
         </div>
       </div>
       <Header />
