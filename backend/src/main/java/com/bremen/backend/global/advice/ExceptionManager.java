6fa0c6ef707a2c6e17a34497be7727c546e8ce76
onyoo <dhsdb02@gmail.com>
2024-05-18 14:37:51 +0900

    refactor(video): #160 영상 첨부 관련 예외처리 추가

--- backend/src/main/java/com/bremen/backend/global/advice/ExceptionManager.java
+++ backend/src/main/java/com/bremen/backend/global/advice/ExceptionManager.java
@@ -8,8 +8,10 @@ import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.multipart.support.MissingServletRequestPartException;
 
 import com.bremen.backend.global.CustomException;
+import com.bremen.backend.global.response.ErrorCode;
 import com.bremen.backend.global.response.ErrorResponse;
 
 @RestControllerAdvice
@@ -33,6 +35,31 @@ public class ExceptionManager {
 	@ExceptionHandler({IOException.class})
 	protected ResponseEntity handleIOException(IOException ex) {
 		return new ResponseEntity(new ErrorResponse(400, ex.getMessage()), HttpStatus.CONFLICT);
+	}
+
+	@ExceptionHandler(MissingServletRequestPartException.class)
+	public ResponseEntity<ErrorResponse> handleMissingServletRequestPartException(MissingServletRequestPartException ex) {
+		String missingPartName = ex.getRequestPartName();
+		ErrorCode errorCode = null;
+		String errorMessage = "";
+
+		switch (missingPartName) {
+			case "video":
+				errorCode = ErrorCode.NO_VIDEO_FILE_ATTACHED;
+				errorMessage = "비디오 파일이 첨부되지 않았습니다.";
+				break;
+			case "thumbnail":
+				errorCode = ErrorCode.NO_THUMBNAIL_FILE_ATTACHED;
+				errorMessage = "썸네일 파일이 첨부되지 않았습니다.";
+				break;
+			case "highlight":
+				errorCode = ErrorCode.NO_HIGHLIGHT_FILE_ATTACHED;
+				errorMessage = "하이라이트 파일이 첨부되지 않았습니다.";
+				break;
+		}
+
+		ErrorResponse errorResponse = new ErrorResponse(errorCode.getStatus(), errorMessage);
+		return ResponseEntity.badRequest().body(errorResponse);
 	}
 
 }
