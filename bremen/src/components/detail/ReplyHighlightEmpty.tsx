389d443aa1379579e891385c83ca968f5648b3b1
seonghyeon.moon <seonghyeon.moon@snu.ac.kr>
2024-05-20 08:31:52 +0900

    fix: #116 댓글 프로필사진 깨짐 현상 수정

--- bremen/src/components/detail/ReplyHighlightEmpty.tsx
+++ bremen/src/components/detail/ReplyHighlightEmpty.tsx
@@ -2,14 +2,14 @@
 import styles from '@/components/detail/ReplyHighlight.module.scss';
 // import Video from '@/components/Common/Video';
 import useUserInfoStore from '@/stores/UserInfo';
+import ProfileImage from '../Common/ProfileImage';
 
 export default function ReplyHighlightEmpty({
   replyHandler,
 }: {
   replyHandler: () => void;
 }) {
-  const {zustandUserImage} = useUserInfoStore.getState();
-  const myProfileImage = zustandUserImage;
+  const {zustandUserImage, zustandUserNickname} = useUserInfoStore.getState();
   return (
     <>
       <div className={styles.cell} onClick={replyHandler}>
@@ -18,7 +18,11 @@ export default function ReplyHighlightEmpty({
         </div>
         <div className={styles.reply}>
           <div className={styles.profileimg}>
-            <img src={myProfileImage} alt="myProfileImage" />
+            {/* <img src={myProfileImage} alt="myProfileImage" /> */}
+            <ProfileImage
+              profileImage={zustandUserImage}
+              userNickname={zustandUserNickname}
+            />
           </div>
           <div>댓글 입력 ...</div>
         </div>
