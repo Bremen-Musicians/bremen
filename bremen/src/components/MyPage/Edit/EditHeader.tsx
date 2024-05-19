2cf365754a6d6f0d429f2196ac3859020445587a
seonghyeon.moon <seonghyeon.moon@snu.ac.kr>
2024-05-19 18:07:51 +0900

    feat: #166 페이지 헤더 뒤로가기 기능 추가

--- bremen/src/components/MyPage/Edit/EditHeader.tsx
+++ bremen/src/components/MyPage/Edit/EditHeader.tsx
@@ -1,10 +1,18 @@
+'use client';
+
 import {FaArrowLeft} from 'react-icons/fa6';
 import styles from '@/components/MyPage/Profile/MyPageHeader.module.scss';
+import {useRouter} from 'next/navigation';
 
 export default function Header() {
+  const router = useRouter();
+  const goBack = () => {
+    router.back();
+  };
+
   return (
     <div className={styles.mypageHeader}>
-      <FaArrowLeft />
+      <FaArrowLeft onClick={goBack} />
       <span>프로필 수정</span>
     </div>
   );
--- bremen/src/components/MyPage/Profile/MyPageHeader.tsx
+++ bremen/src/components/MyPage/Profile/MyPageHeader.tsx
@@ -1,10 +1,16 @@
 import {FaArrowLeft} from 'react-icons/fa6';
 import styles from '@/components/MyPage/Profile/MyPageHeader.module.scss';
+import {useRouter} from 'next/navigation';
 
 export default function Header({nickname}: {nickname: string}) {
+  const router = useRouter();
+  const goBack = () => {
+    router.back();
+  };
+
   return (
     <div className={styles.mypageHeader}>
-      <FaArrowLeft />
+      <FaArrowLeft onClick={goBack} />
       <span>{nickname}</span>
     </div>
   );
